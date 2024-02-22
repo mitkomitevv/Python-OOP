4ca197941854cf99a52253ed2a69a3ae2b2b3e18
Mitko Mitev <onesible@gmail.com>
2024-02-22 14:42:09 +0200

    Create hero.py

---
+++ Lectures/first_steps_in_oop/exercise/hero.py
@@ -0,0 +1,13 @@
+class Hero:
+    def __init__(self, name : str, health):
+        self.name = name
+        self.health = health
+
+    def defend(self, damage):
+        if self.health - damage <= 0:
+            self.health = 0
+            return f"{self.name} was defeated"
+        self.health -= damage
+
+    def heal(self, amount):
+        self.health += amount
