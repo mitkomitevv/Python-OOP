2ecc3d9cf30667236af21dfa851e88726d7d602b
Mitko Mitev <onesible@gmail.com>
2024-02-28 20:55:12 +0200

    adding solutions

---
+++ Lectures/classes_and_objects/exercise/library/project/library.py
@@ -0,0 +1,29 @@
+from user import User
+
+
+class Library:
+    def __init__(self):
+        self.user_records = []
+        self.books_available = {}
+        self.rented_books = {}
+
+    def get_book(self, author: str, book_name: str, days_to_return: int, user: User):
+        if book_name in self.books_available[author]:
+            user.books.append(book_name)
+            self.books_available[author].remove(book_name)
+            if user.username not in self.rented_books:
+                self.rented_books[user.username] = {}
+            self.rented_books[user.username][book_name] = days_to_return
+            return f"{book_name} successfully rented for the next {days_to_return} days!"
+
+        for name, books in self.rented_books.items():
+            if book_name in books:
+                return (f'The book "{book_name}" is already rented and will be available '
+                        f'in {self.rented_books[name][book_name]} days!')
+
+    def return_book(self, author: str, book_name: str, user: User):
+        if book_name not in user.books:
+            return f"{user.username} doesn't have this book in his/her records!"
+        user.books.remove(book_name)
+        self.books_available[author].append(book_name)
+        self.rented_books[user.username].pop(book_name)
