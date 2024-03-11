f52acaef42c97dced34d9132f307585c229a4651
Mitko Mitev <onesible@gmail.com>
2024-03-11 18:16:28 +0200

    added solution

---
+++ Lectures/05_static_and_class_methods/exercise/gym/project/gym.py
@@ -0,0 +1,49 @@
+from typing import List
+from customer import Customer
+from equipment import Equipment
+from exercise_plan import ExercisePlan
+from subscription import Subscription
+from trainer import Trainer
+
+
+class Gym:
+
+    def __init__(self):
+        self.customers: List[Customer] = []
+        self.trainers: List[Trainer] = []
+        self.equipment: List[Equipment] = []
+        self.plans: List[ExercisePlan] = []
+        self.subscriptions: List[Subscription] = []
+
+    def add_customer(self, customer: Customer):
+        if customer not in self.customers:
+            self.customers.append(customer)
+
+    def add_trainer(self, trainer: Trainer):
+        if trainer not in self.trainers:
+            self.trainers.append(trainer)
+
+    def add_equipment(self, equipment: Equipment):
+        if equipment not in self.equipment:
+            self.equipment.append(equipment)
+
+    def add_plan(self, plan: ExercisePlan):
+        if plan not in self.plans:
+            self.plans.append(plan)
+
+    def add_subscription(self, subscription: Subscription):
+        if subscription not in self.subscriptions:
+            self.subscriptions.append(subscription)
+
+    def subscription_info(self, subscription_id: int):
+        subscription = next(filter(lambda s: s.id == subscription_id, self.subscriptions))
+        customer = next(filter(lambda c: c.id == subscription.customer_id, self.customers))
+        trainer = next(filter(lambda t: t.id == subscription.trainer_id, self.trainers))
+        plan = next(filter(lambda e: e.id == subscription.exercise_id, self.plans))
+        equipment = next(filter(lambda e: e.id == plan.equipment_id, self.equipment))
+
+        return (f"{subscription}\n"
+                f"{customer}\n"
+                f"{trainer}\n"
+                f"{equipment}\n"
+                f"{plan}")
