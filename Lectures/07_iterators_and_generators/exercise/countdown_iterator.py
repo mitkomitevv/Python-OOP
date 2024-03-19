9d315bac2fc16cd0cc2fb40dece24ef3aa2ebee7
Mitko Mitev <onesible@gmail.com>
2024-03-19 12:57:45 +0200

    added solution

---
+++ Lectures/07_iterators_and_generators/exercise/countdown_iterator.py
@@ -0,0 +1,20 @@
+class countdown_iterator:
+
+    def __init__(self, count: int):
+        self.count = count + 1
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count == 0:
+            raise StopIteration
+
+        self.count -= 1
+        return self.count
+
+
+iterator = countdown_iterator(0)
+for item in iterator:
+    print(item, end=" ")
